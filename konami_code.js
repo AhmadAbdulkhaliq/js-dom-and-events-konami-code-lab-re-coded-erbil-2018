b9de8923e6366f5b18e09ec4c49c01d044c3ae85
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-09-05 03:43:45 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,24 +2,19 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 
 function init() {
-  
-   // Write your JavaScript code inside the init() function
-   
-  let index = 0;
+  document.body.addEventListener('keydown', function onKeyDownHandler(e) {
+  let index = 0
 
-  document.body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.detail || e.which);
+  let key = parseInt(e.which || e.detail)
 
-    if (code[index] === key) {
-      index++;
+  if (key === code[0]) {
+    index++
+  }
+  if (index === code.length) {
+    allert("Hurray!")
+  } else {
+    index = 0
+  }
 
-      if (index === code.length) {
-        alert('Hurray!');
-
-        index = 0;
-      }
-    } else {
-      index = 0;
-    }
-  });
+})
 }
